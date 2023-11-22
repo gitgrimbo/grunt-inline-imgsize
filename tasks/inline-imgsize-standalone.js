d11488647b99c4fbdff3ebf1d1416ddaea247d85
gitgrimbo <gitgrimbo@gmail.com>
2023-11-22 23:40:06 +0000

    export standalone inlineImgSize function

---
+++ tasks/inline-imgsize-standalone.js
@@ -0,0 +1,56 @@
+var fs        = require('fs'),
+    imagesize = require('imagesize');
+
+module.exports = function inlineImgSize(contents, path) {
+    var Parser = imagesize.Parser;
+    var get_image_dimensions = function (buffer) {
+        var parser = Parser();
+
+        switch (parser.parse(buffer)) {
+            case Parser.EOF:
+                return;
+            case Parser.INVALID:
+                return;
+            case Parser.DONE:
+                return parser.getResult();
+        }
+    };
+
+    var regexes = {
+        // only match local files
+        img: /<img[^\>]+src=['"](?!http:|https:|\/\/|data:image)([^"']+)["'][^\>]*>/gm,
+        src: /src=['"]([^"']+)["']/m,
+        nosize: /\snosize(\s|>)/m,
+        size: /(height|width)=/
+    };
+
+    var matches = contents.match(regexes.img) || [];
+    matches.forEach(function(tag) {
+        // XXX is this necessary?
+        // tag = tag.substring(0, tag.length - 1);
+
+        // skip this img if the size is already specified
+        if (tag.match(regexes.size)) {
+            return;
+        }
+
+        // skip this img if the nosize attr is present
+        if (tag.match(regexes.nosize)) {
+            console.log("NOSIZE", tag);
+            return;
+        }
+
+        var src = tag.match(regexes.src)[1];
+        var imgpath = path.replace(/[^\/]+$/, '') + src;
+        var dimensions = get_image_dimensions(fs.readFileSync(imgpath));
+        if (!dimensions) {
+            return;
+        }
+
+        var replacement = tag.replace(/^<img/, "<img width=" + dimensions.width + " height=" + dimensions.height);
+
+        contents = contents.replace(tag, replacement);
+    });
+
+    return contents;
+}
--- tasks/inline-imgsize.js
+++ tasks/inline-imgsize.js
@@ -1,16 +1,6 @@
-module.exports = function(grunt) {
+var inlineImgSize = require('./inline-imgsize-standalone');
 
-    var fs        = require('fs'),
-        path      = require('path'),
-        imagesize = require('imagesize');
-
-    var regexes = {
-        // only match local files
-        img: /<img[^\>]+src=['"](?!http:|https:|\/\/|data:image)([^"']+)["'][^\>]*>/gm,
-        src: /src=['"]([^"']+)["']/m,
-        nosize: /\snosize(\s|>)/m,
-        size: /(height|width)=/
-    };
+module.exports = function plugin(grunt) {
 
     var fileOptions = {
         encoding: 'utf-8'
@@ -24,20 +14,6 @@ module.exports = function(grunt) {
 
         grunt.util._.extend(options, this.options());
 
-        var Parser = imagesize.Parser;
-        var get_image_dimensions = function (buffer) {
-            var parser = Parser();
-
-            switch (parser.parse(buffer)) {
-                case Parser.EOF:
-                    return;
-                case Parser.INVALID:
-                    return;
-                case Parser.DONE:
-                    return parser.getResult();
-            }
-        };
-
         this.files.forEach(function(f) {
             var src = f.src.filter(function(path) {
                 // Warn on and remove invalid source files (if nonull was set).
@@ -49,35 +25,7 @@ module.exports = function(grunt) {
                 }
             }).map(function(path) {
                 var contents = grunt.file.read(path, fileOptions);
-
-                var matches = contents.match(regexes.img) || [];
-                matches.forEach(function(tag) {
-                    // XXX is this necessary?
-                    // tag = tag.substring(0, tag.length - 1);
-
-                    // skip this img if the size is already specified
-                    if (tag.match(regexes.size)) {
-                        return;
-                    }
-
-                    // skip this img if the nosize attr is present
-                    if (tag.match(regexes.nosize)) {
-                        console.log("NOSIZE", tag);
-                        return;
-                    }
-
-                    var src = tag.match(regexes.src)[1];
-                    var imgpath = path.replace(/[^\/]+$/, '') + src;
-                    var dimensions = get_image_dimensions(fs.readFileSync(imgpath));
-                    if (!dimensions) {
-                        return;
-                    }
-
-                    var replacement = tag.replace(/^<img/, "<img width=" + dimensions.width + " height=" + dimensions.height);
-
-                    contents = contents.replace(tag, replacement);
-                });
-
+                contents = inlineImgSize(contents, path);
                 grunt.file.write(path, contents, fileOptions);
             });
         });
